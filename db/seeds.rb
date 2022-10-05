9a6d52b9e043c5773b8996ff519ef8eaf27fa862
Seth David Gyimah <esec26@gmail.com>
2022-10-05 11:24:46 +0000

    completed lab requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Sam", last_name: "Mona", grade: 0)
